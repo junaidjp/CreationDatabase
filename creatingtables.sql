26e1a8fc2bc47886baa15c3f6ea4db8a613a7cce
junaidpasha <junaidpasha@junaidpasha-PC>
2015-11-08 13:00:49 -0600

    here it is

--- creatingtables.sql
+++ creatingtables.sql
@@ -17,8 +17,37 @@ balance float
 );
 
 
-create tables for Customer 
+create tables for
+Customer 
 
 Employee 
 
 Address 
+
+
+CREATE TABLE customer	
+(
+
+phone varchar(255),
+name varchar(255),
+ssn varchar(255),
+address varchar(255)
+);
+
+CREATE TABLE employee
+(
+
+name varchar(255),
+empid varchar(255),
+ssn varchar(255),
+designation varchar(255)
+);
+
+CREATE TABLE address
+(
+
+city varchar(255),
+streetname varchar(255),
+zipcode varchar(255),
+state varchar(255)
+);
