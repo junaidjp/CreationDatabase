6643ae106de98628b27de461ec8b4d50a8b7a2cc
junaid <junaid.jp@gmail.com>
2015-11-08 12:01:17 -0600

    creation tables sql

---
+++ creatingtables.sql
@@ -0,0 +1,24 @@
+CREATE TABLE Bank
+(
+bankId int NOT NULL PRIMARY KEY,
+bankName varchar(255),
+bankType varchar(255),
+locatiomn varchar(200)
+);
+
+
+
+CREATE TABLE accountInfo
+(
+
+accountno varchar(255),
+accounttype varchar(255),
+balance float
+);
+
+
+create tables for Customer 
+
+Employee 
+
+Address 
